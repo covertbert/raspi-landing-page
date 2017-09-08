795fb8b770bffc0b989e58d316e5cbad747f11d0
Bertie Blackman <blackmanrgh@gmail.com>
2017-09-08 22:35:49 +0100

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  h1(v-text="'Test'")
+</template>
+
+<script type="text/babel">
+  /* ============
+   IndexPage
+   * ============
+   *
+   * todo: add documentation here!
+   */
+
+  export default {
+    name: 'IndexPage',
+
+    extends: {},
+
+    props: {},
+
+    data () {
+      return {}
+    },
+
+    computed: {},
+
+    components: {},
+
+    watch: {},
+
+    methods: {},
+
+    beforeCreate () {
+    },
+
+    mounted () {
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/scss">
+  h1
+    color: red
+</style>
